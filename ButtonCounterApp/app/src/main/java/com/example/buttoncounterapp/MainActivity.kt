0595c1af76aadf6459b93e16a7f338979deeb585
LezinGalibert <lezin.galibert@petricore.com>
2020-09-07 13:37:43 +0200

    Activity logs

--- ButtonCounterApp/app/src/main/java/com/example/buttoncounterapp/MainActivity.kt
+++ ButtonCounterApp/app/src/main/java/com/example/buttoncounterapp/MainActivity.kt
@@ -1,35 +1,81 @@
 package com.example.buttoncounterapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENTS = "TextContent"
 
 class MainActivity : AppCompatActivity() {
 
     private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userInput: EditText = findViewById<EditText>(R.id.textView)
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
         val button: Button = findViewById<Button>(R.id.button)
-        textView = findViewById<TextView>(R.id.textView2)
+        textView = findViewById<TextView>(R.id.textView)
         textView?.text = ""
         textView?.movementMethod = ScrollingMovementMethod()
         userInput.setText("")
 
         button?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick: called")
                 textView?.append(userInput?.text)
                 textView?.append("\n")
                 userInput.text.clear()
 
             }
         })
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart: called")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestore: called")
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedString = savedInstanceState?.getString(TEXT_CONTENTS, "")
+        textView?.text = savedString
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        Log.d(TAG, "onSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+        outState?.putString(TEXT_CONTENTS, textView?.text.toString())
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop: called")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
     }
 }

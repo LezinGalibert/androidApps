e3f82d0030a02a56779a3d42b86469b2f4eeace0
LezinGalibert <lezin.galibert@petricore.com>
2020-09-14 13:23:36 +0200

    Youtube app

---
+++ YoutubePlayer/app/src/main/java/lezingalibert/youtubeplayer/YoutubeActivity.kt
@@ -0,0 +1,33 @@
+package lezingalibert.youtubeplayer
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+
+class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("Not yet implemented")
+    }
+}

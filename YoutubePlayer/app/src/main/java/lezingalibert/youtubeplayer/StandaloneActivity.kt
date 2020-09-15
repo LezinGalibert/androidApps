d8f4acb4b2fc8950bb7dde6dbb37fde6e093426d
LezinGalibert <lezingalibert@gmail.com>
2020-09-15 15:16:21 +0200

    Added play buttons

---
+++ YoutubePlayer/app/src/main/java/lezingalibert/youtubeplayer/StandaloneActivity.kt
@@ -0,0 +1,34 @@
+package lezingalibert.youtubeplayer
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.youtube.player.YouTubeStandalonePlayer
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_standalone.*
+import java.lang.IllegalArgumentException
+
+class StandaloneActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_standalone)
+
+        btnPlaySingle.setOnClickListener(this)
+        btnPlaylist.setOnClickListener(this)
+        btnPlayVideo.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val intent = when (view.id) {
+            R.id.btnPlayVideo -> YouTubeStandalonePlayer.createVideoIntent(
+                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_VIDEO_ID, 0, true, false
+            )
+            R.id.btnPlaylist -> YouTubeStandalonePlayer.createPlaylistIntent(
+                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_PLAYLIST, 0, 0, true, true
+            )
+            else -> throw IllegalArgumentException("Undefined button clicked")
+        }
+
+    }
+}
